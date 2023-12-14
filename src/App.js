7c1507c76599c2485d61dc3a435212d48c73032b
lndsilva <lndsilva@hotmail.com>
2023-12-14 16:29:25 -0300

    componentes de tabelas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+import TabelaHead from "./components/TabelaHead";
+import TabelaFoot from "./components/TabelaFoot";
+import TabelaBody from "./components/TabelaBoby";
+
 function App() {
   return (
     <div>
       <h1>Tabela de livros</h1>
+      <table className="tabela">
+        <TabelaHead />
+        <TabelaFoot />
+        <TabelaBody />
+      </table>
     </div>
   );
 }
---
+++ src/components/TabelaFoot.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TabelaBody = () => (
+  <tfoot>
+    <tr>
+      <td colSpan="4">Este é o rodapé da tabela</td>
+    </tr>
+  </tfoot>
+);
+
+export default TabelaBody;
