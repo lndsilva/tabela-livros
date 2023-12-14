1594cd6561a2e80bef092bae71775175c51f5c34
lndsilva <lndsilva@hotmail.com>
2023-12-14 16:48:47 -0300

    trabalhando com props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import TabelaHead from "./components/TabelaHead";
 import TabelaFoot from "./components/TabelaFoot";
 import TabelaBody from "./components/TabelaBoby";
 
-function App() {
-  return (
-    <div>
-      <h1>Tabela de livros</h1>
+class App extends Component {
+  state = {
+    livros: [
+      {
+        id: "978-85-8522-632-2",
+        titulo: "CSS Grid Layout",
+        autor: "Maurício Samy Silva",
+      },
+      {
+        id: "978-85-7522-677-3",
+        titulo: "Node Essencial",
+        autor: "Ricardo L. Lecheta",
+      },
+      {
+        id: "978-85-7522-512-7",
+        titulo: "Aprendendo Material Designer",
+        autor: "Kyle Mew",
+      },
+    ],
+  };
+  render() {
+    return (
       <table className="tabela">
         <TabelaHead />
         <TabelaFoot />
         <TabelaBody />
       </table>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
